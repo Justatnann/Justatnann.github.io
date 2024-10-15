049b5b3a8e2139cb5f8da61dc83b11bdfe293950
Nannn <atnanarianderson@gmail.com>
2024-10-15 23:50:38 +0700

    deploy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -37,7 +37,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "39aaa0596c1aa57dd806791230a83f17
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "f8d17e87c29ee027a02af8038acedff3",
+"flutter_bootstrap.js": "77fc3b8608f72a1caa9fb253572cc1d8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -45,6 +45,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "39aaa0596c1aa57dd806791230a83f17
 "icons/logo.png": "8a097626ce1a3f1a62f830b02405e220",
 "index.html": "8dbc799088cb43adf0a51305b4324eea",
 "/": "8dbc799088cb43adf0a51305b4324eea",
+"logo.png": "8a097626ce1a3f1a62f830b02405e220",
 "main.dart.js": "6d26f8770d3e0cbc6c82345a3ef3a954",
 "manifest.json": "c2c72c0d50ed91bd431fc655dc5e083e",
 "version.json": "1974c8c8ae37dd8824c42ffa4a3c1f82"};
